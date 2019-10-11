d63dcbfe02db1dc0b8693fcf4eaf6914d2f82086
Grégory NICOL <gnicol@fluksaqua.com>
2019-10-11 15:35:14 +0200

    Build process
    Fin de la première partie de la formation

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        sourceType: "module"
+    },
+    extends: "standard",
+    plugins: ["html"],
+    rules: {
+        indent: ["error", 4]
+    }
+};
