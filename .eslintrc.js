9ad1fca58371ff25bbc54ad52016b077a3cb1e93
Grégory NICOL <gnicol@fluksaqua.com>
2019-10-15 17:06:05 +0200

    feat: styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,82 @@
 module.exports = {
     root: true,
     parserOptions: {
-        sourceType: "module"
+        parser: "babel-eslint"
     },
-    extends: "standard",
+    extends: ["standard", "plugin:vue/recommended"],
     plugins: ["html"],
+    /*rules: {
+        indent: ["error", 4],
+        "vue/html-indent": ["error", 4],
+        "vue/script-indent": [
+            "error",
+            4
+        ]
+    },*/
     rules: {
-        indent: ["error", 4]
-    }
+        "array-element-newline": ["off"],
+        "comma-dangle": ["error", "always-multiline"],
+        "consistent-this": ["error", "self"],
+        "dot-location": ["error", "property"],
+        "dot-notation": [
+            "warn",
+            {
+                allowKeywords: true,
+                allowPattern: "^[a-z]+(_[a-z]+)+$"
+            }
+        ],
+        "function-paren-newline": ["warn", "consistent"],
+        "guard-for-in": ["error"],
+        "implicit-arrow-linebreak": ["warn", "beside"],
+        indent: ["warn", 4],
+        "key-spacing": [
+            "warn",
+            {
+                beforeColon: false,
+                afterColon: true,
+                mode: "strict"
+            }
+        ],
+        "object-curly-spacing": [
+            "error",
+            "always",
+            {
+                arraysInObjects: true,
+                objectsInObjects: true
+            }
+        ],
+        "object-property-newline": [
+            "error",
+            {
+                allowAllPropertiesOnSameLine: true
+            }
+        ],
+        "no-unused-vars": ["warn"],
+        "no-console": [
+            "warn",
+            {
+                allow: ["info", "debug", "warn", "error"]
+            }
+        ],
+        "quote-props": ["error", "as-needed"],
+        quotes: [
+            "error",
+            "single",
+            {
+                avoidEscape: true
+            }
+        ],
+        radix: ["error", "as-needed"],
+        semi: ["error", "never"],
+        "space-before-function-paren": ["error", "never"],
+        "wrap-iife": ["error", "inside"]
+    },
+    overrides: [
+        {
+            files: ["*.vue"],
+            rules: {
+                indent: "off"
+            }
+        }
+    ]
 };
