dead740be94aa479d92036e2826347a5c1c9eb80
Grégory NICOL <gnicol@fluksaqua.com>
2019-10-16 14:38:46 +0200

    feat: history mode

--- src/router.js
+++ src/router.js
@@ -1,13 +1,22 @@
 import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    routes: [{
-        path: '/', component: Category,
-    }],
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Category,
+        },
+        {
+            path: '/login',
+            component: Login,
+        },
+    ],
 })
 
 export default router
