1d6f0e02f75a44b3704ed0cca6a9e54f5b3b7b0c
Grégory NICOL <gnicol@fluksaqua.com>
2019-10-28 17:01:53 +0100

    feat: JWT

--- src/app.service.js
+++ src/app.service.js
@@ -10,6 +10,16 @@ const appService = {
             })
         })
     },
+    login(credentials) {
+        return new Promise((resolve, reject) => {
+            axios.post('/services/auth.php', credentials)
+                .then(response => {
+                    resolve(response.data)
+                }).catch(response => {
+                    reject(response.status)
+                })
+        })
+    },
 }
 
 export default appService
