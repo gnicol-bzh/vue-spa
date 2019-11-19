4ce0e0ffc5f80deec330f9b534a9a2ee25fbc401
Grégory NICOL <gnicol@fluksaqua.com>
2019-11-19 15:40:59 +0100

    test: testing component changes

--- test/unit/specs/Post.spec.js
+++ test/unit/specs/Post.spec.js
@@ -3,13 +3,28 @@ import Post from '../../../src/theme/Post.vue'
 import { expect } from 'chai'
 
 describe('Post.vue', () => {
-    it('should render th link', () => {
+    const createComponent = () => {
         const PostConstructor = Vue.extend(Post)
-        const comp = new PostConstructor({
+        return new PostConstructor({
             propsData: {
                 link: 'http://www.pluralsight.com',
             },
         }).$mount()
+    }
+    it('should render th link', () => {
+        const comp = createComponent()
         expect(comp.$el.querySelector('.card-footer-item').getAttribute('href')).to.equal('http://www.pluralsight.com')
+    })
+
+    it('should update element\'s href when property link changes', (done) => {
+        const comp = createComponent()
+        expect(comp.$el.querySelector('.card-footer-item').getAttribute('href')).to.equal('http://www.pluralsight.com')
+
+        comp.link = 'http://www.google.com'
+
+        Vue.nextTick(() => {
+            expect(comp.$el.querySelector('.card-footer-item').getAttribute('href')).to.equal('http://www.google.com')
+            done()
+        })
     })
 })
